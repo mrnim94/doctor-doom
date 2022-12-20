02d126c8d2f34d8dce2801e8bb6f5dd46249e447
mr nim <mrnim@nimtechnology.com>
2022-12-20 16:41:55 +0700

    update module gopkg.in/yaml.v2 to v3

--- create_test_dir.go
+++ create_test_dir.go
@@ -1,40 +1,40 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"log"
-// 	"math/rand"
-// 	"os"
-// )
-
-// // Create a directory with the given name in the current directory.
-// //
-// // The directory is created with the permissions 0755.
-// //
-// // It will have 10 levels of subdirectories.
-// //
-// // It each subdirectory will have 100 files with random names. and random content size between 10K and 100K.
-// func CreateDir() {
-// 	dirName := "tmp"
-// 	os.Mkdir(dirName, 0755)
-// 	for i := 0; i < 10; i++ {
-// 		subDirName := fmt.Sprintf("%s/%d", dirName, i)
-// 		os.Mkdir(subDirName, 0755)
-// 		for j := 0; j < 100; j++ {
-// 			fileName := fmt.Sprintf("%s/%d", subDirName, rand.Intn(100000))
-// 			file, err := os.Create(fileName)
-// 			if err != nil {
-// 				log.Fatal(err)
-// 			}
-// 			defer file.Close()
-// 			fileSize := rand.Intn(100000) + 10000
-// 			for k := 0; k < fileSize; k++ {
-// 				file.WriteString("a")
-// 			}
-// 		}
-// 	}
-// }
-
-// func main() {
-// 	CreateDir()
-// }
+//import (
+//	"fmt"
+//	"log"
+//	"math/rand"
+//	"os"
+//)
+//
+//// Create a directory with the given name in the current directory.
+////
+//// The directory is created with the permissions 0755.
+////
+//// It will have 10 levels of subdirectories.
+////
+//// It each subdirectory will have 100 files with random names. and random content size between 10K and 100K.
+//func CreateDir() {
+//	dirName := "tmp"
+//	os.Mkdir(dirName, 0755)
+//	for i := 0; i < 10; i++ {
+//		subDirName := fmt.Sprintf("%s/%d", dirName, i)
+//		os.Mkdir(subDirName, 0755)
+//		for j := 0; j < 100; j++ {
+//			fileName := fmt.Sprintf("%s/%d", subDirName, rand.Intn(100000))
+//			file, err := os.Create(fileName)
+//			if err != nil {
+//				log.Fatal(err)
+//			}
+//			defer file.Close()
+//			fileSize := rand.Intn(100000) + 10000
+//			for k := 0; k < fileSize; k++ {
+//				file.WriteString("a")
+//			}
+//		}
+//	}
+//}
+//
+//func main() {
+//	CreateDir()
+//}
--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/urfave/cli/v2 v2.23.7
 	go.uber.org/zap v1.24.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
-	gopkg.in/yaml.v2 v2.4.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
