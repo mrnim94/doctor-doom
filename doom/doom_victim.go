3a6c39727da46a969283506bcd2303beb19498af
mr nim <mrnim@nimtechnology.com>
2024-01-17 11:30:10 +0700

    initial v2

--- doom/doom_victim.go
+++
@@ -1,9 +0,0 @@
-package doom
-
-type DoomVictim struct {
-	Path             string `json:"path"`
-	Name             string `json:"name"`
-	Size             int64  `json:"size"`
-	LastModifiedUnix int64  `json:"last_modified_unix"`
-	LiveIn           string `json:"live_in"`
-}
